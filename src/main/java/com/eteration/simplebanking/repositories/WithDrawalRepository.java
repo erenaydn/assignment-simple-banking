67a0af026019c3daff184a9449a2334f2f943262
Eren Aydın <83093241+erenaydn@users.noreply.github.com>
2024-02-10 16:50:07 +0300

    Delete WithDrawalRepository.java

--- src/main/java/com/eteration/simplebanking/repositories/WithDrawalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eteration.simplebanking.repositories;
-
-import com.eteration.simplebanking.model.WithdrawalTransaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WithDrawalRepository extends JpaRepository<WithdrawalTransaction, Long> {
-}
